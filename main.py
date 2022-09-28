65aeb267dd20723050f6fe87dc918e4b894f4d11
Nguyễn Minh Giảng <giangbk109@gmail.com>
2022-09-28 20:23:10 +0700

    get data from nutritionix API

--- main.py
+++ main.py
@@ -3,5 +3,34 @@ import requests
 APP_ID = "61f13d61"
 API_KEY = "c23b22b28c8acb6bf4ce7f61e9ca80e1"
 
+MY_AGE = 21
+MY_GENDER = "male"
+MY_WEIGHT = 67
+MY_HEIGHT = 173
+
+
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+nutritionix_header = {
+    "x-app-id":APP_ID,
+    "x-app-key":API_KEY,
+    # "x-remote-user-id":0,
+}
+
+
+exercise = input("Tell me what exercise you did: ")
+
+post_data = {
+    "query":exercise,
+    "gender":MY_GENDER,
+    "weight_kg":MY_WEIGHT,
+    "height_cm":MY_HEIGHT,
+    "age":MY_AGE,
+}
+
+reponses = requests.post(url=nutritionix_endpoint, json=post_data, headers=nutritionix_header)
+exercise_data = reponses.json()
+print(exercise_data)
+
 
 
